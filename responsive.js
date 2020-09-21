b066d692b70c11bedfef2ede5020a7f3761096b4
milipede7 <evidal@saic.edu>
2020-09-20 21:30:54 -0500

    inline-flex,font size,backgrounds,responsive,s/o opensource

---
+++ responsive.js
@@ -0,0 +1,11 @@
+//experimenting with javascript to create a responsive site
+
+let w = window.innerWidth;
+let h = window.innerHeight;
+
+function setup() {
+  }
+
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
